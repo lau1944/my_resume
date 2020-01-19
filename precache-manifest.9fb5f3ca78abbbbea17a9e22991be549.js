e8c8c0621cbc8982c25967e70a1105164f2b8078
liuquan <lauchuen94@gmail.com>
2020-01-19 22:51:01 +0800

    Updates

---
+++ precache-manifest.9fb5f3ca78abbbbea17a9e22991be549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb79a1a03240e6741e0de9b2b5b8f40",
+    "url": "/my_resume/index.html"
+  },
+  {
+    "revision": "a215c4ab017458bcd181",
+    "url": "/my_resume/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8c745d0cc085ffb81a48",
+    "url": "/my_resume/static/js/2.122c0458.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my_resume/static/js/2.122c0458.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a215c4ab017458bcd181",
+    "url": "/my_resume/static/js/main.35bd214b.chunk.js"
+  },
+  {
+    "revision": "6b9d000fb7b002ae6900",
+    "url": "/my_resume/static/js/runtime-main.09f02658.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
